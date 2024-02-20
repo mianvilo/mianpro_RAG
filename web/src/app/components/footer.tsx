6616857493899ec1c51eaf2d4aa6084a8679bda7
markidos <52153481+sviphaige@users.noreply.github.com>
2024-02-20 11:01:08 +0800

    Update footer.tsx

--- web/src/app/components/footer.tsx
+++ web/src/app/components/footer.tsx
@@ -6,7 +6,6 @@ export const Footer: FC = () => {
     <div className="text-center flex flex-col items-center text-xs text-zinc-700 gap-1">
       <div className="text-zinc-400">三顿出品，更懂你的搜索</div>
       <div className="text-zinc-400">三顿半+大模型赋能</div>
-        <div>共建AI搜索新生态</div>
       </div>
 
       <div className="flex items-center justify-center flex-wrap gap-x-4 gap-y-2 mt-2 text-zinc-400">
